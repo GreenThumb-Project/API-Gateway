071f02202546d43be472f40d0f76afea85d9aee5
Diyorbek Ne'matov <diyorbeknematov0321@gmail.com>
2024-06-30 17:30:03 +0500

    Garden maganment serivce uchun API-GATEWAY methodlari yozildi

--- api/api.go
+++ api/api.go
@@ -1,1 +1,13 @@
 package api
+
+import (
+	"api-gateway-service/api/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Router(hand *handler.Handler) *gin.Engine {
+	router := gin.Default()
+
+	return router
+}
