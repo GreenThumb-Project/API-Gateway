0d85c9edc2d0decccf01020f6c569cc1b2cbac43
bekzod <qochqarovbekzod049@gmail.com>
2024-06-30 18:18:25 +0500

    ipa getway yozish boshlandi

--- api/handler/user.go
+++ api/handler/user.go
@@ -7,17 +7,77 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func (h *Handler) GetUserByIdHandler(c *gin.Context) {
+	userId := c.Param("userId")
 
-func (h *Handler)  GetUserByIdHandler(c *gin.Context){
-	userId:=c.Param("userId")
-
-	resp,err:=h.User.GetUserById(c,&pb.GetUserByIdRequest{UserId: userId})
+	resp, err := h.User.GetUserById(c, &pb.GetUserByIdRequest{UserId: userId})
 
 	if err != nil {
-		c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, resp)
+}
+
+func (h *Handler) CreateUserHandler(c *gin.Context) {
+	var user pb.CreateUsersRequest
+	err := c.BindJSON(&user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	resp, err := h.User.CreateUser(c, &user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, resp)
+}
+
+func (h *Handler) DeleteUserHandler(c *gin.Context) {
+	userId:=c.Param("userId")
+
+	resp,err:=h.User.DeleteUser(c,&pb.DeleteUserRequest{UserId: userId})
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	c.JSON(http.StatusOK,resp)
-	return
+}
+
+func (h *Handler)  GetUserByIdProfileHandler(c *gin.Context) {
+	userId:=c.Param("userId")
+
+	resp,err:=h.User.GetUserByIdProfile(c,&pb.GetUserByIdProfileRequest{UserId: userId})
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK,resp)
+}
+
+func (h *Handler) UpdateUserProfileHandler(c *gin.Context)  {
+	var userProfil pb.UpdateUserProfileRequest
+	err:=c.BindJSON(&userProfil)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	resp,err:=h.User.UpdateUserProfile(c,&userProfil)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK,resp)
+
 }
