d215207ca9750f7e93a0c700e7ebbfd13a91999b
bekzod <qochqarovbekzod049@gmail.com>
2024-06-30 16:56:30 +0500

    ipa getway yozish boshlandi

--- api/handler/handler.go
+++ api/handler/handler.go
@@ -1,1 +1,21 @@
 package handler
+
+import (
+	"api-gateway-service/generated/gardenManagement"
+	"api-gateway-service/generated/users"
+)
+
+
+type Handler struct{
+	User users.UserManagementClient
+	Garden gardenManagement.GardenManagementClient
+}
+
+
+func NewHendler(user users.UserManagementClient, garden gardenManagement.GardenManagementClient) *Handler {
+	
+	return &Handler{
+		User: user,
+		Garden: garden,
+	}
+}
